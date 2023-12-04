fe4bfff7fd5cbf5c4bba172127fc7dce51ab5c3e
Axel Cotón Gutiérrez <133801080+AxelCotonGutierrez@users.noreply.github.com>
2023-12-04 16:13:01 +0100

    js

--- assets/js/cursos.js
+++ assets/js/cursos.js
@@ -1,8 +1,10 @@
+// Array para almacenar los botones activos
+var botonesActivos = [];
+
 function toggleBoton(botonId) {
     var boton = document.getElementById('activar_' + botonId);
     var filas = document.querySelectorAll('.fila_oculta[data-codigos*="' + botonId + '"]');
-    var contenedorPrincipal = document.getElementById('contenedor-principal');
-
+    
     // Verifica si el botón actual está activo
     var botonActivo = boton.classList.contains('on');
 
@@ -11,12 +13,33 @@ function toggleBoton(botonId) {
         fila.style.display = botonActivo ? 'none' : 'table-row';
     });
 
-    // Cambia el estado del botón
+    // Cambia el estado del botón y actualiza el array de botones activos
     boton.classList.toggle('on');
+    if (botonActivo) {
+        var index = botonesActivos.indexOf(botonId);
+        if (index !== -1) {
+            botonesActivos.splice(index, 1);
+        }
+    } else {
+        botonesActivos.push(botonId);
+    }
 
-    // Verifica si hay al menos un botón activo
-    var algunBotonActivo = document.querySelector('.boton_menu_cursos.on');
+    // Llama a la función para gestionar la visibilidad de las filas
+    gestionarVisibilidadFilas();
+}
 
-    // Agrega o quita la clase según el estado de los botones
-    contenedorPrincipal.classList.toggle('sin-borde', !algunBotonActivo);
+// Función para gestionar la visibilidad de las filas
+function gestionarVisibilidadFilas() {
+    var filas = document.querySelectorAll('.fila_oculta');
+    
+    filas.forEach(function (fila) {
+        var codigos = fila.getAttribute('data-codigos').split(' ');
+        
+        // Verifica si alguna fila depende de un botón activo
+        var filaVisible = codigos.some(function (codigo) {
+            return botonesActivos.includes(codigo);
+        });
+        
+        fila.style.display = filaVisible ? 'table-row' : 'none';
+    });
 }
