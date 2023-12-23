01713f55384dbfecc4e24f935d3153d72b8413e2
Axel Cotón Gutiérrez <133801080+AxelCotonGutierrez@users.noreply.github.com>
2023-12-23 20:14:42 +0100

    tags 2

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,4 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+function redirectToTagPage(tag) {
+    localStorage.setItem('selectedTag', tag);
+    window.location.href = '/learningmathematicas/tags/';
+  }
+  
+  // El resto del código que maneja la lógica en la página de tags
+  document.addEventListener('DOMContentLoaded', function() {
     var selectedTag = localStorage.getItem('selectedTag');
     if (selectedTag) {
       var selectedDiv = document.getElementById(selectedTag);
