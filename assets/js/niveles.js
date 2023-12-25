68665bf97d3ed35efeb2eeb8252b4b3c5c120b49
Axel Cotón Gutiérrez <133801080+AxelCotonGutierrez@users.noreply.github.com>
2023-12-25 17:36:00 +0100

    js77

--- assets/js/niveles.js
+++ assets/js/niveles.js
@@ -9,22 +9,37 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('#accordion .card-link, #accordion .level-link').forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault(); // Previene cualquier desplazamiento
-            var sectionId = this.getAttribute('data-target');
+            var sectionId = this.getAttribute('data-target') || this.getAttribute('href').substring(1);
             toggleSection(sectionId);
         });
     });
 });
 
 function showLevel(levelId) {
-    // Ocultar todos los niveles
-    document.querySelectorAll('.level-content').forEach(level => {
-        level.classList.remove('show');
+    // Encontrar el nivel y las secciones padre correspondientes
+    var levelSection = document.getElementById(levelId);
+    var parentSections = getParentSections(levelSection);
+
+    // Mostrar todas las secciones padre y el nivel
+    parentSections.forEach(section => {
+        if (section && !section.classList.contains('show')) {
+            section.classList.add('show');
+        }
     });
-    // Mostrar el nivel seleccionado
-    var selectedLevel = document.getElementById(levelId);
-    if (selectedLevel) {
-        selectedLevel.classList.add('show');
+    if (levelSection && !levelSection.classList.contains('show')) {
+        levelSection.classList.add('show');
     }
+}
+
+function getParentSections(element) {
+    var parents = [];
+    while (element) {
+        if (element.matches('.collapse')) {
+            parents.push(element);
+        }
+        element = element.parentElement;
+    }
+    return parents.reverse();
 }
 
 function toggleSection(sectionId) {
