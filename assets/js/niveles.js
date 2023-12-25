ece0703466b6a1950f2228f0e2591ef170312230
Axel Cotón Gutiérrez <133801080+AxelCotonGutierrez@users.noreply.github.com>
2023-12-25 16:46:21 +0100

    Niveles try 1

---
+++ assets/js/niveles.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Verificar si la URL tiene un fragmento (por ejemplo, #nivel1)
+    var hash = window.location.hash;
+    if (hash) {
+        showLevel(hash.substring(1));
+    }
+
+    // Evento de clic para mostrar/ocultar secciones
+    document.querySelectorAll('.level-group a').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            toggleSection(this.getAttribute('href').substring(1));
+        });
+    });
+});
+
+function showLevel(levelId) {
+    // Ocultar todos los niveles
+    document.querySelectorAll('.level-content').forEach(level => {
+        level.classList.remove('show');
+    });
+    // Mostrar el nivel seleccionado
+    var selectedLevel = document.getElementById(levelId);
+    if (selectedLevel) {
+        selectedLevel.classList.add('show');
+    }
+}
+
+function toggleSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.classList.toggle('show');
+}
