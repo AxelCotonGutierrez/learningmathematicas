09f586d3756f33a02b3be0f054d3072cdb521312
Axel Cotón Gutiérrez <133801080+AxelCotonGutierrez@users.noreply.github.com>
2023-12-25 17:45:25 +0100

    cambio de tercio

--- assets/js/niveles.js
+++ assets/js/niveles.js
@@ -1,50 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Verificar si la URL tiene un fragmento (por ejemplo, #level-1)
-    var hash = window.location.hash;
-    if (hash) {
-        showLevel(hash.substring(1));
-    }
-
-    // Evento de clic para mostrar/ocultar secciones
-    document.querySelectorAll('#accordion .card-link, #accordion .level-link').forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault(); // Previene cualquier desplazamiento
-            var sectionId = this.getAttribute('data-target') || this.getAttribute('href').substring(1);
-            toggleSection(sectionId);
-        });
-    });
-});
-
-function showLevel(levelId) {
-    // Encontrar el nivel y las secciones padre correspondientes
-    var levelSection = document.getElementById(levelId);
-    var parentSections = getParentSections(levelSection);
-
-    // Mostrar todas las secciones padre y el nivel
-    parentSections.forEach(section => {
-        if (section && !section.classList.contains('show')) {
-            section.classList.add('show');
-        }
-    });
-    if (levelSection && !levelSection.classList.contains('show')) {
-        levelSection.classList.add('show');
+function toggleSubLevels(subLevelId) {
+    var subLevel = document.getElementById(subLevelId);
+    if (subLevel.style.display === "none" || !subLevel.style.display) {
+        subLevel.style.display = "block";
+    } else {
+        subLevel.style.display = "none";
     }
 }
 
-function getParentSections(element) {
-    var parents = [];
-    while (element) {
-        if (element.matches('.collapse')) {
-            parents.push(element);
-        }
-        element = element.parentElement;
-    }
-    return parents.reverse();
-}
-
-function toggleSection(sectionId) {
-    var section = document.getElementById(sectionId);
-    if (section) {
-        section.classList.toggle('show');
+function toggleLevelDetails(levelId) {
+    var levelDetail = document.getElementById(levelId);
+    if (levelDetail.style.display === "none" || !levelDetail.style.display) {
+        levelDetail.style.display = "block";
+    } else {
+        levelDetail.style.display = "none";
     }
 }
