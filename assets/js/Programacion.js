24085d1125cbfaa408e41203bee447c083870e5b
Axel Cotón Gutiérrez <133801080+AxelCotonGutierrez@users.noreply.github.com>
2024-01-09 10:46:51 +0100

    js

--- assets/js/Programacion.js
+++ assets/js/Programacion.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Escuchadores de eventos para los niveles primarios y secundarios
-    document.querySelectorAll('.nivel-primario, .nivel-secundario').forEach(function(nivel) {
-        nivel.addEventListener('click', function() {
-            // Encuentra el nivel o subnivel asociado y alterna su visibilidad
+    // Escuchadores de eventos para los niveles primarios
+    document.querySelectorAll('.nivel-primario').forEach(function(nivelPrimario) {
+        nivelPrimario.addEventListener('click', function() {
+            // Ocultar todos los niveles secundarios, terciarios y detalles
+            document.querySelectorAll('.nivel-secundario, .nivel-terciario').forEach(function(element) {
+                element.style.display = 'none';
+            });
+            document.querySelectorAll('[class$="-detalle"]').forEach(function(detail) {
+                detail.style.display = 'none';
+            });
+
+            // Mostrar solo el nivel secundario relacionado
+            var targetId = this.getAttribute('data-target');
+            toggleSubLevels(targetId, true);
+        });
+    });
+
+    // Escuchadores de eventos para los niveles secundarios
+    document.querySelectorAll('.nivel-secundario').forEach(function(nivelSecundario) {
+        nivelSecundario.addEventListener('click', function() {
             var targetId = this.getAttribute('data-target');
             toggleSubLevels(targetId);
         });
@@ -11,17 +27,22 @@ document.addEventListener('DOMContentLoaded', function() {
     // Escuchador de eventos para todos los niveles terciarios
     document.querySelectorAll('.nivel-terciario').forEach(function(nivelTerciario) {
         nivelTerciario.addEventListener('click', function() {
-            // Encuentra y alterna los detalles asociados con este nivel terciario
             var detallesClase = nivelTerciario.id + '-detalle';
             toggleLevelDetails(detallesClase);
         });
     });
 });
 
-function toggleSubLevels(subLevelId) {
+function toggleSubLevels(subLevelId, isPrimary = false) {
     var subLevel = document.getElementById(subLevelId);
     if (subLevel) {
-        subLevel.style.display = subLevel.style.display === "none" ? "table-row" : "none";
+        if (isPrimary) {
+            // Si es nivel primario, mostrar directamente el subnivel
+            subLevel.style.display = "table-row";
+        } else {
+            // Para niveles secundarios, alternar la visibilidad
+            subLevel.style.display = subLevel.style.display === "none" ? "table-row" : "none";
+        }
     }
 }
 
