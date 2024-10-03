2522a234d2781fb295d000a6ac2ca6645ee0fce1
AxelCotonGutierrez <learningmathematicas@gmail.com>
2024-10-03 10:00:18 +0200

    footer

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,40 +1,54 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Código del encabezado
-    const headerHTML = `
-    <div class="menu">
-        <button class="menu-toggle">☰</button>
-        <ul>
-            <li><a href="https://axelcotongutierrez.github.io/learningmathematicas/">
-                <img src="cb.jpg"> 
-            </a></li>
-            <li><a href="/learningmathematicas/projects/Cursos/">Cursos y Materias</a></li>
-            <li><a href="/learningmathematicas/projects/Niveles/">Niveles</a></li>
-            <li><a href="/learningmathematicas/">Entradas</a></li>
-            <li><a href="/learningmathematicas/tags">Etiquetas</a></li>
-            <li><a href="/learningmathematicas/about">Blog</a></li>
-            <li><a href="#">Utilidades</a>
-                <ul>
-                    <li><a href="/learningmathematicas/projects/Utilidades/Glosario.html">Glosario</a></li>
-                    <li><a href="/learningmathematicas/projects/Utilidades/Programacion.html">Programación</a></li>
-                    <li><a href="/learningmathematicas/projects/Utilidades/Soroban.html">Soroban</a></li>
-                </ul>
-            </li>
-        </ul>
-    </div>
-    <a href="https://axelcotongutierrez.github.io/learningmathematicas/">
-        <img src="https://axelcotongutierrez.github.io/learningmathematicas/assets/images//top.jpg" alt="Blog Mathematica" style="display: block; margin: 0 auto; width: 100%; height: auto;">
-    </a>
-    `;
-    
-    const headerElement = document.createElement('header');
-    headerElement.innerHTML = headerHTML;
-    document.body.insertBefore(headerElement, document.body.firstChild);
+// Código del encabezado
+const headerHTML = `
+<div class="menu">
+    <button class="menu-toggle">☰</button>
+    <ul>
+        <li><a href="https://axelcotongutierrez.github.io/learningmathematicas/">
+            <img src="cb.jpg"> 
+        </a></li>
+        <li><a href="/learningmathematicas/projects/Cursos/">Cursos y Materias</a></li>
+        <li><a href="/learningmathematicas/projects/Niveles/">Niveles</a></li>
+        <li><a href="/learningmathematicas/">Entradas</a></li>
+        <li><a href="/learningmathematicas/tags">Etiquetas</a></li>
+        <li><a href="/learningmathematicas/about">Blog</a></li>
+        <li><a href="#">Utilidades</a>
+            <ul>
+                <li><a href="/learningmathematicas/projects/Utilidades/Glosario.html">Glosario</a></li>
+                <li><a href="/learningmathematicas/projects/Utilidades/Programacion.html">Programación</a></li>
+                <li><a href="/learningmathematicas/projects/Utilidades/Soroban.html">Soroban</a></li>
+            </ul>
+        </li>
+    </ul>
+</div>
+<a href="https://axelcotongutierrez.github.io/learningmathematicas/">
+    <img src="https://axelcotongutierrez.github.io/learningmathematicas/assets/images//top.jpg" alt="Blog Mathematica" style="display: block; margin: 0 auto; width: 100%; height: auto;">
+</a>
+`;
 
-    // Navegación: Lógica para el menú
-    var menuToggle = document.querySelector('.menu-toggle');
-    var menu = document.querySelector('.menu');
-      
-    menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
+// Código del pie de página
+const footerHTML = `
+<footer class="footer">
+    <ul class="footer-links">
+        <li><a href="http://github.com/AxelCotonGutierrez"> <img src="githubinverseicon.jpg"></a></li>
+        <li><a href="https://www.youtube.com/@learningmathematicas"> <img src="youtubeinverseicon.jpg"></i></a></li>
+    </ul>
+</footer>
+`;
+
+// Insertar el encabezado en el documento
+const headerElement = document.createElement('header');
+headerElement.innerHTML = headerHTML;
+document.body.insertBefore(headerElement, document.body.firstChild);
+
+// Insertar el pie de página en el documento
+const footerElement = document.createElement('footer');
+footerElement.innerHTML = footerHTML;
+document.body.appendChild(footerElement);  // Coloca el footer al final del body
+
+// Navegación: Lógica para el menú
+var menuToggle = document.querySelector('.menu-toggle');
+var menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', function() {
+    menu.classList.toggle('active');
 });
