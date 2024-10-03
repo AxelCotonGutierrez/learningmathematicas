95b34b39938a4999dd53d6880c8f3d3c06c7a46d
AxelCotonGutierrez <learningmathematicas@gmail.com>
2024-10-03 10:17:39 +0200

    header footer

--- assets/js/header.js
+++ assets/js/header.js
@@ -25,34 +25,36 @@ document.addEventListener("DOMContentLoaded", function() {
         <img src="https://axelcotongutierrez.github.io/learningmathematicas/assets/images//top.jpg" alt="Blog Mathematica" style="display: block; margin: 0 auto; width: 100%; height: auto;">
     </a>
     `;
-    
-    const headerElement = document.createElement('header');
-    headerElement.innerHTML = headerHTML;
-    document.body.insertBefore(headerElement, document.body.firstChild);
+
+    // Código del pie de página
+    const footerHTML = `
+    <footer class="footer">
+        <ul class="footer-links">
+            <li><a href="http://github.com/AxelCotonGutierrez"> <img src="githubinverseicon.jpg"></a></li>
+            <li><a href="https://www.youtube.com/@learningmathematicas"> <img src="youtubeinverseicon.jpg"></i></a></li>
+        </ul>
+    </footer>
+    `;
+
+    // Insertar el encabezado en el placeholder
+    const headerPlaceholder = document.getElementById('header-placeholder');
+    if (headerPlaceholder) {
+        headerPlaceholder.innerHTML = headerHTML;
+    }
+
+    // Insertar el pie de página en el placeholder
+    const footerPlaceholder = document.getElementById('footer-placeholder');
+    if (footerPlaceholder) {
+        footerPlaceholder.innerHTML = footerHTML;
+    }
 
     // Navegación: Lógica para el menú
     var menuToggle = document.querySelector('.menu-toggle');
     var menu = document.querySelector('.menu');
       
-    menuToggle.addEventListener('click', function() {
-        menu.classList.toggle('active');
-    });
+    if (menuToggle && menu) {
+        menuToggle.addEventListener('click', function() {
+            menu.classList.toggle('active');
+        });
+    }
 });
-
-
-// Código del pie de página
-const footerHTML = `
-<footer class="footer">
-    <ul class="footer-links">
-        <li><a href="http://github.com/AxelCotonGutierrez"> <img src="githubinverseicon.jpg"></a></li>
-        <li><a href="https://www.youtube.com/@learningmathematicas"> <img src="youtubeinverseicon.jpg"></i></a></li>
-    </ul>
-</footer>
-`;
-
-
-// Insertar el pie de página en el placeholder
-const footerPlaceholder = document.getElementById('footer-placeholder');
-if (footerPlaceholder) {
-    footerPlaceholder.innerHTML = footerHTML;
-}
